570b6c388beab619315ac820e1034bcd4a183c9d
suriyaprakash <75439259+suriyarichard@users.noreply.github.com>
2023-01-08 02:56:31 +0530

    ui

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -1,17 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sago_user/widgets/Listfiles.dart';
+import 'package:sago_user/widgets/profile.dart';
 
-class Homescreen extends StatelessWidget {
-  const Homescreen({super.key});
+class Homescreen extends StatefulWidget {
+  Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  int _selectedIndex = 0;
+  final screens = [const dataFiles(), const profileScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User"),
+        title: Text("Sago User"),
       ),
-      body: Column(children: [Text("Data"), Row()]),
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white12,
+        mouseCursor: SystemMouseCursors.grab,
+        selectedIconTheme: IconThemeData(color: Colors.indigo, size: 40),
+        selectedItemColor: Colors.indigo,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.file_open_outlined),
+            label: 'Files',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle),
+            label: 'Profile',
+          ),
+        ],
+      ),
+
+      //New
     );
   }
 }
---
+++ lib/widgets/Listfiles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class dataFiles extends StatefulWidget {
+  const dataFiles({super.key});
+
+  @override
+  State<dataFiles> createState() => _dataFilesState();
+}
+
+class _dataFilesState extends State<dataFiles> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text("sksknnnnnnnnnnn"),
+    ));
+  }
+}
